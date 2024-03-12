9590653ba1a72464f20c11a22253370c5c529886
Prathamesh2502 <prathameshdchaudhari09@gmial.com>
2024-03-12 19:47:11 +0530

    first commit

--- frontend/src/components/Navbaar.js
+++ frontend/src/components/Navbaar.js
@@ -38,13 +38,56 @@ const Navbaar = ({login}) => {
       ];
     }
   };
+  const loginStatusMobile =()=>{
+      const token = localStorage.getItem("jwt");
+      if (login || token) {
+        return [
+          <>
+              
+            <Link to="/">
+              <li><span class="material-symbols-outlined">
+                 home
+               </span></li>
+            </Link>
+            <Link to="/profile"><span class="material-symbols-outlined">
+                account_circle</span></Link>
+            <Link to="/createPost"><li><span class="material-symbols-outlined">
+                      add</span></li></Link>
+            <Link style={{marginLeft:"20px"}} to="/Myfollowingpost">
+              <li><span class="material-symbols-outlined">
+                  explore</span></li></Link>
+            <Link >
+              <li onClick={()=>{
+                setModalopen(true)
+              }}><span class="material-symbols-outlined">
+               logout
+              </span></li></Link>
+  
+            </>,
+        ];
+      } else {
+        return [
+          <>
+            <Link to="/signup">
+              <li>SignUp</li>
+            </Link>
+            <Link to="/signin">
+              <li>SignIn</li>
+            </Link>
+          </>,
+        ];
+      }
+  };
   return (
     <div className='navbar'>
-      <img src={logo} alt="" onClick={()=>{
+      <img id="insta-logo" src={logo} alt="" onClick={()=>{
         navigate("/");
       }}/>
       <ul className='nav-menu'>
       {loginStatus()}
+      </ul>
+      <ul className='nav-mobile'>
+      {loginStatusMobile()}
       </ul>
     </div>
   )
